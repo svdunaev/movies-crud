1267d2a434b85048fd05058742a87fef243249b8
Stanislav <svdunaev95@gmail.com>
2025-12-10 17:00:04 +0500

    add get movies and get movies by id methods

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module movies-crud
 
 go 1.25.0
+
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -1,32 +1,69 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
 type Movie struct {
-	title    string
-	director string
-	id       int64
-	genre    string
+	Title    string `json:"title"`
+	Director string `json:"director"`
+	Id       string `json:"id"`
+	Genre    string `json:"genre"`
 }
 
-var movies []Movie
+var movies = []Movie{
+	{
+		Title:    "Terminator",
+		Director: "James Cameron",
+		Id:       uuid.NewString(),
+		Genre:    "Sci-Fi",
+	},
+}
 
 func main() {
 	fmt.Println("Start server")
 	defer fmt.Println("Stop server")
-
-	movies = append(movies, Movie{
-		title:    "Terminator",
-		director: "James Cameron",
-		id:       1,
-		genre:    "Sci-Fi",
-	})
+	http.HandleFunc("GET /movies", getMoviesHandler)
+	http.HandleFunc("GET /movies/{id}", getMovieByIdHandler)
 
 	fmt.Println(movies)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+// TODO: modify to find by id
+func getMovieById(id string) *Movie {
+	for i := range movies {
+		if movies[i].Title == id {
+			return &movies[i]
+		}
+	}
+
+	return nil
+}
+
+func getMoviesHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	fmt.Println("This is from get handler", movies)
+	json.NewEncoder(res).Encode(movies)
+}
+
+func getMovieByIdHandler(res http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+	parts := strings.Split(path, "/")
+
+	id := parts[2]
+	movie := getMovieById(id)
+
+	fmt.Println(movie)
+
+	res.Header().Set("Content-Type", "application/json")
+	fmt.Println("This is from get by id handler", movie)
+	json.NewEncoder(res).Encode(movie)
 }
