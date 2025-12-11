26d49407d1a990e2614f0ec020b64b21a1bbe1ff
Stanislav <svdunaev95@gmail.com>
2025-12-11 15:23:09 +0500

    refactor to use map instead of slice, add request validation

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"slices"
 	"strings"
 
 	"github.com/google/uuid"
@@ -14,15 +13,13 @@ import (
 type Movie struct {
 	Title    string `json:"title"`
 	Director string `json:"director"`
-	Id       string `json:"id"`
 	Genre    string `json:"genre"`
 }
 
-var movies = []Movie{
-	{
+var movies = map[string]Movie{
+	uuid.NewString(): {
 		Title:    "Terminator",
 		Director: "James Cameron",
-		Id:       uuid.NewString(),
 		Genre:    "Sci-Fi",
 	},
 }
@@ -36,24 +33,14 @@ func main() {
 	http.HandleFunc("DELETE /movies/{id}", deleteMovieHandler)
 	http.HandleFunc("PUT /movies/{id}", updateMovieHandler)
 
-	fmt.Println(movies)
-
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
-func getMovieById(id string) (idx int, movie *Movie) {
-	for i := range movies {
-		if movies[i].Id == id {
-			return i, &movies[i]
-		}
-	}
-
-	return -1, nil
-}
-
 func getMoviesHandler(res http.ResponseWriter, req *http.Request) {
+	if len(movies) < 1 {
+		fmt.Fprintf(res, "There are no movies in this list, you can add the first one")
+	}
 	res.Header().Set("Content-Type", "application/json")
-	fmt.Println("This is from get handler", movies)
 	json.NewEncoder(res).Encode(movies)
 }
 
@@ -61,28 +48,31 @@ func getMovieByIdHandler(res http.ResponseWriter, req *http.Request) {
 	path := req.URL.Path
 	parts := strings.Split(path, "/")
 	id := parts[2]
-	_, movie := getMovieById(id)
+	movie, ok := movies[id]
+	if !ok {
+		http.Error(res, "No such movie exist", http.StatusNotFound)
+		return
+	}
 
 	res.Header().Set("Content-Type", "application/json")
-	fmt.Println("This is from get by id handler", movie)
 	json.NewEncoder(res).Encode(movie)
 }
 
-// todo: add duplicate validation
 func postMovieHandler(res http.ResponseWriter, req *http.Request) {
 	decoder := json.NewDecoder(req.Body)
 	var movie Movie
 	err := decoder.Decode(&movie)
 	if err != nil {
-		panic(err)
+		http.Error(res, "Bad request", http.StatusBadRequest)
 	}
 
-	movie.Id = uuid.NewString()
-
-	movies = append(movies, movie)
+	if movieExists(movies, movie.Title) {
+		http.Error(res, "This movie already exists", http.StatusConflict)
+	} else {
+		movies[uuid.NewString()] = movie
+	}
 
 	res.Header().Set("Content-Type", "application/json")
-	fmt.Println("This is from post handler", movies)
 	json.NewEncoder(res).Encode(movies)
 }
 
@@ -91,15 +81,14 @@ func deleteMovieHandler(res http.ResponseWriter, req *http.Request) {
 	parts := strings.Split(path, "/")
 	id := parts[2]
 
-	idx, _ := getMovieById(id)
-	if idx >= 0 {
-		movies = slices.Delete(movies, idx, idx+1)
+	_, ok := movies[id]
+	if ok {
+		delete(movies, id)
 	} else {
-		log.Fatal("this movie does not exist")
+		http.Error(res, "No such movie exist", http.StatusNotFound)
 	}
 
 	res.Header().Set("Content-Type", "application/json")
-	fmt.Println("This is from delete handler", movies)
 	json.NewEncoder(res).Encode(movies)
 }
 
@@ -108,20 +97,31 @@ func updateMovieHandler(res http.ResponseWriter, req *http.Request) {
 	parts := strings.Split(path, "/")
 	id := parts[2]
 
-	idx, movie := getMovieById(id)
-
 	var updated Movie
 	decoder := json.NewDecoder(req.Body)
 	err := decoder.Decode(&updated)
 	if err != nil {
-		panic(err)
+		http.Error(res, "Bad reques", http.StatusBadRequest)
 	}
 
-	updated.Id = movie.Id
+	_, ok := movies[id]
 
-	movies[idx] = updated
+	if !ok {
+		http.Error(res, "No such movie exist", http.StatusNotFound)
+	}
+
+	movies[id] = updated
 
 	res.Header().Set("Content-Type", "application/json")
-	fmt.Println("This is from put handler", movies)
 	json.NewEncoder(res).Encode(movies)
+}
+
+func movieExists(movies map[string]Movie, title string) bool {
+	for _, movie := range movies {
+		if movie.Title == title {
+			return true
+		}
+	}
+
+	return false
 }
